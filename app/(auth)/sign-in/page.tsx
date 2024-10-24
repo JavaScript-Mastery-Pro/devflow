f1bd58f3349ed33682c3d5827d33073fdcd72945
sujatagunale <sujata@jsmastery.pro>
2024-10-24 19:54:47 +0530

    authentication_auth-form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
 
-const SignIn = () => {
-  return <div>SignIn</div>;
-};
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
+const SignIn = () => (
+  <AuthForm
+    formType="SIGN_IN"
+    schema={SignInSchema}
+    defaultValues={{ email: "", password: "" }}
+    onSubmit={(data) => Promise.resolve({ success: true, data })}
+  />
+);
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
 
-const SignUp = () => {
-  return <div>SignUp</div>;
-};
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
+const SignUp = () => (
+  <AuthForm
+    formType="SIGN_UP"
+    schema={SignUpSchema}
+    defaultValues={{ username: "", name: "", email: "", password: "" }}
+    onSubmit={(data) => Promise.resolve({ success: true, data })}
+  />
+);
 
 export default SignUp;
