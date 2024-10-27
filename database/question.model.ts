24266bc038c973b9c9352a5d6e31901bfe3510f9
sujatagunale <sujata@jsmastery.pro>
2024-10-27 23:14:46 +0530

    develop-backend_question-model

---
+++ database/question.model.ts
@@ -0,0 +1,28 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IQuestion extends Document {
+  title: string;
+  content: string;
+  views: number;
+  answers: number;
+  upvotes: number;
+  downvotes: number;
+  author: Schema.Types.ObjectId;
+  createdAt: Date;
+}
+
+const QuestionSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  views: { type: Number, default: 0 },
+  answers: { type: Number, default: 0 },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Question = models.Question || model("Question", QuestionSchema);
+
+export default Question;
