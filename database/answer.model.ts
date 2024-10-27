1cfb15cbb6341fa028b5b6d68c7e3f7dcb8cfcba
sujatagunale <sujata@jsmastery.pro>
2024-10-27 23:18:27 +0530

    develop-backend_answer-model

---
+++ database/answer.model.ts
@@ -0,0 +1,23 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IAnswer extends Document {
+  author: Schema.Types.ObjectId;
+  question: Schema.Types.ObjectId;
+  content: string;
+  upvotes: number;
+  downvotes: number;
+  createdAt: Date;
+}
+
+const AnswerSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  content: { type: String, required: true },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Answer = models.Answer || model("Answer", AnswerSchema);
+
+export default Answer;
