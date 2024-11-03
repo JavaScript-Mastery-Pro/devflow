3779ae553eb788a88f968de32cfcc12d023a03ef
sujatagunale <sujata@jsmastery.pro>
2024-11-03 20:19:10 +0530

    minimal conditional logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,46 +1,12 @@
 import "server-only";
-import pino, { Logger, LoggerOptions } from "pino";
 
-interface SerializedError {
-  type?: string;
-  message: string;
-  stack?: string;
-  code?: string | number;
-  name: string;
-}
-
-interface ExtendedError extends Error {
-  type?: string;
-  code?: string | number;
-}
+import pino from "pino";
 
 const isEdge = process.env.NEXT_RUNTIME === "edge";
 const isProduction = process.env.NODE_ENV === "production";
-const logLevel: string =
-  process.env.LOG_LEVEL || (isProduction ? "warn" : "debug");
 
-const errorSerializer = (err: ExtendedError): SerializedError => {
-  return {
-    type: err.type,
-    message: err.message,
-    stack: err.stack,
-    code: err.code,
-    name: err.name,
-  };
-};
-
-const loggerOptions: LoggerOptions = {
-  level: logLevel,
-  formatters: {
-    level: (label) => ({ level: label.toUpperCase() }),
-    log: (obj) => {
-      if (obj.err instanceof Error) {
-        return { ...obj, err: errorSerializer(obj.err) };
-      }
-      return obj;
-    },
-  },
-  timestamp: pino.stdTimeFunctions.isoTime,
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
   transport:
     !isEdge && !isProduction
       ? {
@@ -49,12 +15,15 @@ const loggerOptions: LoggerOptions = {
             colorize: true,
             ignore: "pid,hostname",
             translateTime: "SYS:standard",
-            messageFormat: "[{level}] {msg}",
           },
         }
       : undefined,
-};
-
-const logger: Logger = pino(loggerOptions);
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
 
 export default logger;
