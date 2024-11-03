787969b8df045ebe7ef089ae7029e882a2b8eca8
sujatagunale <sujata@jsmastery.pro>
2024-11-03 19:44:55 +0530

    ad transport back

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,14 +2,14 @@ import pino from "pino";
 
 const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-  // transport: {
-  //   target: "pino-pretty",
-  //   options: {
-  //     colorize: true,
-  //     ignore: "pid,hostname",
-  //     translateTime: true,
-  //   },
-  // },
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      ignore: "pid,hostname",
+      translateTime: true,
+    },
+  },
   formatters: {
     level: (label) => {
       return { level: label.toUpperCase() };
