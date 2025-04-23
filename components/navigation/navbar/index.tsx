057bbbc44dfde47c962bf8e34044ee7cc3e3c21f
sujatagunale <sujata@jsmastery.pro>
2025-04-23 17:02:45 +0530

    feat: global search

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { auth } from "@/auth";
+import GlobalSearch from "@/components/search/GlobalSearch";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
 
@@ -26,7 +27,7 @@ const Navbar = async () => {
         </p>
       </Link>
 
-      <p>Global Search</p>
+      <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
